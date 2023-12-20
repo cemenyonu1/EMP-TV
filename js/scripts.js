775cf39f79060a657b77eb112551ac0deeaa68a9
cemenyonu1 <149470860+cemenyonu1@users.noreply.github.com>
2023-12-20 16:34:25 -0700

    Added 3 different Pokemon and their objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World');
-
-let favoriteFood= 'steak';
-document.write(favoriteFood);
+let pokemonList= [
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
+    {name: 'Gyarados', height: 6.5, type: ['water', 'flying']}
+];
