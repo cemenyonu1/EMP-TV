e70d37cc8b6af09a2a74eb7a044998bd5f4fc0ba
cemenyonu1 <149470860+cemenyonu1@users.noreply.github.com>
2023-12-30 16:50:46 -0700

    Updated Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList= [
-    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']}
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']}
+    {name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},
+    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
     {name: 'Gyarados', height: 6.5, type: ['water', 'flying']}
 ];
