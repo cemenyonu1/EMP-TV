2bcbccabf23ea65bdcb2c2c04f46aecba3d45343
cemenyonu1 <149470860+cemenyonu1@users.noreply.github.com>
2023-12-30 18:16:04 -0700

    Added the display messages for pokemon names and heights

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ let pokemonList= [
     {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
     {name: 'Gyarados', height: 6.5, type: ['water', 'flying']}
 ];
+
+for (let i = 0;  pokemonList.length; i++) {
+    // This will display the pokemon names with their heights
+    document.write(pokemonList[i].name + ` (height:  + ${pokemonList[i].height} )` )
+
+if (pokemonList[i].height >= 7) {
+    // This will display a message for large pokemons
+    document.write(' That\'s a big pokemon! ')
+} else if (pokemonList[i].height > 3 && pokemonList[i].height < 7) {
+    // This will display a message for medium pokemons
+    document.write(' That\'s a medium sized pokemon! ')
+} else {
+    // This will display a message for small pokemons
+    document.write(' That\'s a small pokemon! ')
+}
+}
